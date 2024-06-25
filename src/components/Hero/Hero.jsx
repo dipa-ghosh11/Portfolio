9fc2eeabd8303134159fc6e7335597fb895795b5
Dipa-44 <dipaghosh0902@gmail.com>
2024-06-25 21:46:56 +0530

    Skill section updated

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
+import React from "react";
 import styles from "./Hero.module.css";
-import {getImageUrl} from "../../utlis";
-
+import { getImageUrl } from "../../utlis";
 
 export const Hero = () => {
   return (
     <section className={styles.container}>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Hi, I'm Dipa</h1>
-            <p className={styles.description}>I am a versatile Fontend web developer with a confident grasp of various technologies and frameworks.</p>
-            <div className='btn'> 
-              <a href="mailto:dipaghosh0902@gmail.com" className={styles.contactBtn}>Contact Me</a>
-              <a href="https://drive.google.com/file/d/1AXQL3bJPCPjEkB4VDBTJMJeCprzjauNp/view?usp=sharing" className={styles.contactBtn1}>Download Cv</a>
-            </div> 
-
+      <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Dipa</h1>
+        <p className={styles.description}>
+          I am a versatile Fontend web developer with a confident grasp of
+          various technologies and frameworks.
+        </p>
+        <div className={styles.btn}>
+          <a
+            href="mailto:dipaghosh0902@gmail.com"
+            className={styles.contactBtn}
+          >
+            Contact Me
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1vsTueOrV9raN6y6iLnAkhqU5Lx07fkGE/view?usp=sharing"
+            className={styles.contactBtn}
+          >
+            Download Cv
+          </a>
         </div>
-        <img src= {getImageUrl("hero/heroImage.jpg")} alt="image of me"  className={styles.heroImg}/>
-        <div className={styles.topBlur}></div>
-        <div className={styles.bottomBlur}></div>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.jpg")}
+        alt="image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur}></div>
+      <div className={styles.bottomBlur}></div>
     </section>
-  )
-}
+  );
+};
